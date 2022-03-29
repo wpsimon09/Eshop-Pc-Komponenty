da59095455d797036aa495273dc8775ceb0534f0
Šimon <95319163+wpsimon09@users.noreply.github.com>
2022-03-29 18:27:03 +0200

    Add files via upload

---
+++ NaplnenieTabulkyUzivatelov.sql
@@ -0,0 +1,21 @@
+use eShopPCKomponenty
+
+INSERT INTO dbo.Uzivatel
+( 
+    meno,priezvisko,ulica,cisloDomu,mesto,PSC,email,TelCislo
+)
+VALUES
+( 
+    'Matej','Bedanr','Kankalinkova',12,'Prešov',08001,'matej.bednar@gmail.com',0980980
+),
+(
+    'Jozef','Alois','Francuzka',14,'Poprad',05801,'jozef.alois@gmail.com',0980980
+),
+(
+    'Simona','Elemkova','Modra' ,100,'Stropkov' ,09101, 'simona.elemkova@gmail.com',098098
+),
+(
+    'Jarmila','Petrova','Cervena',50,'Košice', 0401,'jarmila.petrova@gmail.com',0988098
+)
+GO
+

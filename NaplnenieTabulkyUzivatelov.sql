fe52f48d2a37b50bbccc0c57820d26a151ce9b58
Šimon <95319163+wpsimon09@users.noreply.github.com>
2022-04-07 18:31:50 +0200

    Update NaplnenieTabulkyUzivatelov.sql

--- NaplnenieTabulkyUzivatelov.sql
+++ NaplnenieTabulkyUzivatelov.sql
@@ -16,6 +16,73 @@ VALUES
 ),
 (
     'Jarmila','Petrova','Cervena',50,'Košice', 0401,'jarmila.petrova@gmail.com',0988098
+),
+(
+    'Miriam', 'Sladká','Gerojová',18,'Poprad',05801,'miriam.sladka@gmail.com',098098098
+),
+(
+    'Jaroslav','Rýchly','Soniková',30,'Prešov',08001,'jaroslav.rýchyl@gmail.com',098098098
+),
+(
+    'František','Straka','Bednárová',40,'Sobrance',09123,'feri.straka@gmail.com',098098888
 )
 GO
+
+INSERT INTO dbo.Uzivatel
+( 
+    meno,priezvisko,ulica,cisloDomu,mesto,PSC,email,TelCislo
+)
+VALUES
+(
+    'Marta','Sladka','Slavková',39,'Prešov',08001,'marta.sladka@gmail.com',0906372118
+),
+(
+    'Slávka','Fialkova','Tovarišská',10,'Bratislava',81103,'slavka.fialkova@gmail.com',0906342131
+),
+(
+    'Vojtech','Fišar','Pocitacova',22,'Veľký šariš',08221,'vojtech.fisar@gmail.com',0906512248
+),
+(
+    'Jaroslav','Sbec','Twitchova',12,'Michalovce',07101,'jaroslav.sbec@gmail.com',0906323118
+),
+(
+    'Lacko','Holub','Severná',69,'Prešov',08001,'lacko.holub@gmail.com',0906372981
+),
+(
+    'Vladimira','Ukecana','Canva',88,'Kosice',04001,'vladimira.ukecana@gmail.com',0906748928
+),
+(
+    'Fratiska','Prava','Michalovska',19,'Stropkov',09101,'frantiska.prava@gmail.com',090684387
+),
+(
+    'Slavka','Lava','Levocska',12,'Prešov',08001,'slavka.lava@gmail.com',0906234229
+),
+(
+    'Gabriela','Galova','Konova',10,'Zilina',01001,'gabriela.galova@gmail.com',092129973
+)
+GO
+--'','','',66,'',08001,'@gmail.com',0906323451
+INSERT INTO dbo.Uzivatel
+( 
+    meno,priezvisko,ulica,cisloDomu,mesto,PSC,email,TelCislo
+)
+VALUES
+(
+    'Kamil','Jelen','Americka',66,'Banska Bystrica',97401,'kamil.jelen@gmail.com',0906372118
+),
+(
+    'Ivan','Pekny','Ruska',73,'Slavkov',97401,'ivan.pekny@gmail.com',0906786877
+),
+(
+    'Gabriel','Skaredy','Gastonksa',10,'Kosice',040001,'gabriel.skaredy@gmail.com',0906372771
+),
+(
+    'James','Heatfield','Francuzka',32,'Kokosovce',08252,'james.heatfield@gmail.com',0906726717
+),
+(
+    'Helenka','Vondrackova','Zbirkova',11,'Bratislava',08001,'helenka.vondrackova@gmail.com',0906323451
+)
+
+GO
+
 
